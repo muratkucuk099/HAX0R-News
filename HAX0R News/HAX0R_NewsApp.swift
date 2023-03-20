d32274ea7469cb604ff76e221fcec67836de8c1a
muratkucuk099 <muratkucuk099@gmail.com>
2023-03-20 15:22:21 +0300

    Initial Commit

---
+++ HAX0R News/HAX0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAX0R_NewsApp.swift
+//  HAX0R News
+//
+//  Created by Mac on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct HAX0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HAX0R NewsUITests/HAX0R_NewsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HAX0R_NewsUITestsLaunchTests.swift
+//  HAX0R NewsUITests
+//
+//  Created by Mac on 20.03.2023.
+//
+
+import XCTest
+
+final class HAX0R_NewsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
